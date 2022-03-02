1d3426809797f14ee7cc20d9164737f3d3214920
Aniket <aniketdhokane2071@gmail.com>
2022-03-02 23:45:32 +0530

    Links

--- src/App.js
+++ src/App.js
@@ -3,13 +3,22 @@ import {Routes, Route} from 'react-router-dom'
 import './App.css';
 import About from './components/About';
 import Home from './components/Home';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <Routes>
+    <>
+    <Navbar/>
+
+
+     <Routes>
       <Route path="/" element={<Home/>}></Route>
       <Route path="/about" element={<About/>}></Route>
     </Routes>
+
+
+    </>
+   
  
   );
 }
