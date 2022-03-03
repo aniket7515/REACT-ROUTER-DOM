8813a8daec12aaab033ea7db78223f64254654c9
Aniket <aniketdhokane2071@gmail.com>
2022-03-03 17:11:57 +0530

    Nested Routes

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import {Routes, Route} from 'react-router-dom'
 
 import './App.css';
 import About from './components/About';
+import FeaturedProduct from './components/FeaturedProduct';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
+import NewProduct from './components/NewProduct';
 import NoMatch from './components/NoMatch';
 import OrderSummary from './components/OrderSummary';
+import Product from './components/Product';
 
 function App() {
   return (
@@ -17,6 +20,10 @@ function App() {
       <Route path="/" element={<Home/>}></Route>
       <Route path="/about" element={<About/>}></Route>
       <Route path="/order-summary" element={<OrderSummary/>}></Route>
+      <Route path="/products" element={<Product/>}>
+        <Route path="featured" element={<FeaturedProduct/>} />
+        <Route path="new" element={<NewProduct/>}/>
+      </Route>
       <Route path="*" element={<NoMatch/>}></Route>
     </Routes>
 
---
+++ src/components/NewProduct.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NewProduct = () => {
+  return (
+    <div>List of NewProduct</div>
+  )
+}
+
+export default NewProduct
