a06641e792a8066dfbdad9731a9e09bab404c801
Aniket <aniketdhokane2071@gmail.com>
2022-03-03 16:48:43 +0530

    No Match Route

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import About from './components/About';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
+import NoMatch from './components/NoMatch';
 import OrderSummary from './components/OrderSummary';
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
       <Route path="/" element={<Home/>}></Route>
       <Route path="/about" element={<About/>}></Route>
       <Route path="/order-summary" element={<OrderSummary/>}></Route>
+      <Route path="*" element={<NoMatch/>}></Route>
     </Routes>
 
 
