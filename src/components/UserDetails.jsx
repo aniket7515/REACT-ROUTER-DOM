e693eff7887f8fe0b8dc0ce5d9c543ad78e1cd99
Aniket <aniketdhokane2071@gmail.com>
2022-03-04 16:08:04 +0530

    URL Params

--- src/components/UserDetails.jsx
+++ src/components/UserDetails.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import {useParams} from 'react-router-dom'
 
 const UserDetails = () => {
+    // Method 1
+    // const params= useParams()
+    // const userId= params.userId
+
+    // Method 2
+    const {userId}=useParams()
+
   return (
     <div>
-        Details about User
+        Details about User {userId}
     </div>
   )
 }
